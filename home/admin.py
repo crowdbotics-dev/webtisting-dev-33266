000c6c0c7a1537aedb8993169dfce60533ec094e
Crowdbotics <engineering@crowdbotics.com>
2022-09-13 06:12:08 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Asdfgd
+
+admin.site.register(Asdfgd)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Asdfgd(models.Model):
+    "Generated Model"
+    nemwwr = models.BigIntegerField()
